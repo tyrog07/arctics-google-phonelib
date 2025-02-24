f75d008c86fc7ade0e74617da46ea4a5bc2c068f
Rajat Ninawe <rajatninawe123@gmail.com>
2025-02-24 15:59:42 +0530

    added eslint config

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+// eslint.config.js
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+      globals: {
+        require: 'readonly', // Define "require" as a global variable
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'error',
+      eqeqeq: 'error', // Enforce the use of === and !==
+      curly: 'error', // Enforce consistent brace style for all control statements
+      'no-undef': 'error', // Disallow the use of undeclared variables
+      quotes: ['error', 'single'], // Enforce the consistent use of single quotes
+      semi: ['error', 'always'], // Require semicolons at the end of statements
+      indent: ['error', 2], // Enforce consistent indentation of 2 spaces
+      'linebreak-style': ['error', 'unix'], // Enforce consistent linebreak style
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-function-return-type': 'warn', // Require explicit return types on functions and class methods
+      '@typescript-eslint/no-explicit-any': 'warn', // Disallow the use of `any` type
+      'no-console': 'error', // Add this line to disallow console statements
+      eqeqeq: 'error', // Enforce the use of === and !==
+      curly: 'error', // Enforce consistent brace style for all control statements
+      'no-undef': 'error', // Disallow the use of undeclared variables
+      quotes: ['error', 'single'], // Enforce the consistent use of single quotes
+      semi: ['error', 'always'], // Require semicolons at the end of statements
+      indent: ['error', 2], // Enforce consistent indentation of 2 spaces
+      'linebreak-style': ['error', 'unix'], // Enforce consistent linebreak style
+    },
+  },
+  // Common configurations
+  ...compat.extends('eslint:recommended'),
+  ...compat.plugins('jest', '@typescript-eslint/eslint-plugin'),
+  {
+    files: ['**/*.js', '**/*.ts'],
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'packages/google-phonelib-js/glib/',
+      'packages/google-phonelib-js/src/template/',
+      'packages/google-phonelib-js/src/providers/',
+      'packages/google-phonelib-js/dist/',
+      'packages/google-phonelib-js/glib/',
+    ],
+  },
+];
--- packages/google-phonelib-js/build/build.js
+++ packages/google-phonelib-js/build/build.js
@@ -1,26 +1,29 @@
-const esbuild = require("esbuild");
-const { readFileSync, writeFileSync } = require("fs");
-const { brotliCompressSync } = require("zlib");
+const esbuild = require('esbuild');
+const { readFileSync, writeFileSync } = require('fs');
+const { brotliCompressSync } = require('zlib');
 
 // Build configuration for CommonJS
 esbuild
   .build({
-    entryPoints: ["src/index.js"], // Entry point for your application
+    entryPoints: ['src/index.js'], // Entry point for your application
     bundle: true, // Bundle all dependencies
     minify: true, // Minify the output files
-    format: "cjs", // Output format (CommonJS)
-    outdir: "dist", // Output directory for CommonJS
+    format: 'cjs', // Output format (CommonJS)
+    outdir: 'dist', // Output directory for CommonJS
     sourcemap: true, // Generate sourcemaps
     metafile: true, // Generate metafile for analysis
-    target: ["node12"], // Target Node.js 12 and above
+    target: ['node12'], // Target Node.js 12 and above
   })
   .then(() => {
     // Optionally compress files with Brotli
-    const files = ["dist/index.js"];
+    const files = ['dist/index.js'];
     files.forEach((file) => {
       const contents = readFileSync(file);
       const compressed = brotliCompressSync(contents);
       writeFileSync(`${file}.br`, compressed);
     });
   })
-  .catch(() => process.exit(1));
+  .catch(() => {
+    //eslint-disable-next-line
+    process.exit(1);
+  });
